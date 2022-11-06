5bf6fb65de32d137703119c6c4f308ea08a6de35
Till Hainbach <till@deepfrozenstudios.de>
2022-11-06 11:21:26 +0100

    ci(deploy): delete table on remove

--- stacks/StorageStack.ts
+++ stacks/StorageStack.ts
@@ -1,6 +1,6 @@
 import { Bucket, Table } from '@serverless-stack/resources';
-import type { Stack, App } from '@serverless-stack/resources';
 import type { StackFnProps } from './types';
+import * as cdk from 'aws-cdk-lib';
 
 export function StorageStack({ stack, app }: StackFnProps) {
   const bucket = new Bucket(stack, 'Uploads');
@@ -11,6 +11,13 @@ export function StorageStack({ stack, app }: StackFnProps) {
       noteId: 'string',
     },
     primaryIndex: { partitionKey: 'userId', sortKey: 'noteId' },
+    cdk: {
+      table: {
+        // don't keep this table after clean up
+        // since it's just a demo app
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+      },
+    },
   });
 
   return { table, bucket };
